43ffc155b29b1f3de3e2e281b4ef47f6d1369d47
MelinaGomez <mat.gomez@alumno.um.edu.ar>
2024-04-27 09:31:52 -0300

    Commit para main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from persona import Persona                                                # -> para importar clase
+
+if __name__ == '__main__':
+    persona = Persona()
+    print(persona.__str__())
+
+
+    persona = Persona()
+    print(persona)                                                           # -> 2 direcciones de memoria distintas
